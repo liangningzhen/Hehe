2ff0305bc1bc22e435ce27303ab9dff76e1844c5
liangningzhen <liangningzhen@sina.com>
2015-01-31 13:40:00 +0800

    twice commit

--- src/com/hehe/MainActivity.java
+++ src/com/hehe/MainActivity.java
@@ -5,6 +5,8 @@ import butterknife.InjectView;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -17,9 +19,36 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
         ButterKnife.inject(this);
         
-        setSupportActionBar(mToolbar);
+        if(mToolbar != null) {
+        	setSupportActionBar(mToolbar);
+        	mToolbar.setTitle("呵呵");
+        }
+        getSupportActionBar().setTitle("呵呵");
         
-        
+    }
+    
+    
+    
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+      // Inflate the menu; this adds items to the action bar if it is present.
+      getMenuInflater().inflate(R.menu.main, menu);
+      return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+      // Handle action bar item clicks here. The action bar will
+      // automatically handle clicks on the Home/Up button, so long
+      // as you specify a parent activity in AndroidManifest.xml.
+      int id = item.getItemId();
+
+      //noinspection SimplifiableIfStatement
+      if (id == R.id.action_settings) {
+        return true;
+      }
+
+      return super.onOptionsItemSelected(item);
     }
 
 }
